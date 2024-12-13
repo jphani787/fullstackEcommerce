5c2297ea032396881d8d4749c68fd8bbe0c34e55
jphani787 <jphanindra787@gmail.com>
2024-12-13 15:47:58 +0530

    json parsing middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,10 @@
-import express from "express";
+import express, {json, urlencoded} from "express";
 import productRouter from "./routes/products/index";
 
 const port = 3000;
 const app = express();
-
+app.use(urlencoded({extended: true}));
+app.use(json());
 app.get('/', (req, res)=>{
     res.send('hello world 787 YOY!!');
 });
